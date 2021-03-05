0c5e29eceacd6fdbf746373b0bcad04fee65cef8
SeungYeong <lsyeong93@naver.com>
2021-03-05 20:53:47 +0900

    add youtube API

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,27 @@
 import './app.css';
 
+import React, { useEffect, useState } from 'react';
+import VideoList from './compoments/video_list/video_list';
+
 function App() {
-  return <h1>hello</h1>;
-}
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCwCUk7-O8nUFPR_a5_Lj3JXh7IjCwEg8Q", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return (
+    <>
+      <VideoList videos={videos} />
+    </>
+  );
+};
 
 export default App;
